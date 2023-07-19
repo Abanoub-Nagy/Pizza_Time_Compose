019c26a8b3a435ea3036bebf9dfa681a84f3460d
Abanoub <abanoubnagy24@gmail.com>
2023-07-20 00:49:37 +0300

    finish pizza time task animation

--- app/src/main/java/com/example/pizza_time_compose/MainActivity.kt
+++ app/src/main/java/com/example/pizza_time_compose/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.pizza_time_compose.screen.PizzaScreen
 import com.example.pizza_time_compose.ui.theme.Pizza_Time_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +16,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pizza_Time_ComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PizzaScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pizza_Time_ComposeTheme {
-        Greeting("Android")
     }
 }
